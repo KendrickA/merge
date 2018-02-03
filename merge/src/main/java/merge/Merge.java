0452b87525289d57c2b8ba19fbf3496fb9b1c8a6
Andy Kendrick <andrewkendrick45@gmail.com>
2018-02-03 23:13:34 +0000

    commi4

--- merge/src/main/java/merge/Merge.java
+++ merge/src/main/java/merge/Merge.java
@@ -13,7 +13,7 @@ public class Merge {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// my comments more commentat
+		// my comments more commentater
 		for (int i=0;i<10;i++){
 			
 			System.out.println("Mege Test");
