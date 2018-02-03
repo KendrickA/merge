d511aa2a7b6ab3874a48775833cde6da68714e77
Andy Kendrick <andrewkendrick45@gmail.com>
2018-02-03 17:50:30 +0000

    merge1

---
+++ merge/src/main/java/merge/Merge.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package merge;
+
+/**
+ * @author user
+ *
+ */
+public class Merge {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// my comments 
+		for (int i=0;i<10;i++){
+			
+			System.out.println("Mege Test");
+			
+		}
+
+	}
+
+}
