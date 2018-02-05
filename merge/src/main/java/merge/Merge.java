3b8f258a4769be00f49d99207072912ce374a9e4
Andy Kendrick <andrewkendrick45@gmail.com>
2018-02-05 15:48:18 +0000

    commit new

--- merge/src/main/java/merge/Merge.java
+++ merge/src/main/java/merge/Merge.java
@@ -15,6 +15,7 @@ public class Merge {
 	public static void main(String[] args) {
 		// my comments more commentater
 		// this is on the the new branch addcaps
+		// the new stuff
 		for (int i=0;i<10;i++){
 			
 			System.out.println("Mege Test");
