7a65a0fb13b6df68d585fb83f11c4ac1e0455a5f
Andy Kendrick <andrewkendrick45@gmail.com>
2018-02-06 09:13:21 +0000

    comm6

---
+++ merge/src/main/java/merge/Merge3.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package merge;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author user
+ *
+ */
+public class Merge3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		List<Integer> ints = new ArrayList<Integer>();
+		ints.add(1);
+		
+	
+		// TODO Auto-generated method stub
+		for (Integer intVal : ints){
+			System.out.println("Mege Test frep change change on branch");
+			System.out.println(intVal.toString());
+		}
+
+	}
+
+}
